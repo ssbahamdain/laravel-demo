8f1079ec4d2e307d71d2cda11360a8acce0f6e5e
Slavey Karadzhov <slavey.karadzhov@zend.com>
2014-09-17 11:43:38 +0200

    Added code to deploy using two packages.

--- scripts/post_stage.php
+++ scripts/post_stage.php
@@ -47,4 +47,14 @@ if(file_exists($envFolder) && is_dir($envFolder)) {
 }
 
 
- foreach ($_ENV as $key=> $value) { if (0 === ($pos = strpos($key, 'ZS_COMPOSER_'))) { putenv(substr($key, $pos),$value); } } copy(__DIR__ . '/composer.json', getenv('ZS_APPLICATION_BASE_DIR') . '/composer.json'); $cwd = getcwd(); chdir(__DIR__); $phpBin = "/usr/local/zend/bin/php"; if (defined('PHP_BINARY')) { $phpBin = PHP_BINARY; } $command = 'post-install-cmd'; if (getenv('ZS_PREVIOUS_APP_VERSION')) { $command = 'post-update-cmd'; } shell_exec("$phpBin composer.phar run-script $command -n -d " . getenv('ZS_APPLICATION_BASE_DIR')); unlink(getenv('ZS_APPLICATION_BASE_DIR') . '/composer.json '); chdir($cwd);
+# get location of the laraveldemovendor library
+$vendor = zend_deployment_library_path('laraveldemovendor');
+# make symlink to that folder
+symlink($vendor, getenv('ZS_APPLICATION_BASE_DIR').'/vendor/');
+
+/*
+# get location of the laraveldemovendor library
+$assets = zend_deployment_library_path('laraveldemoassets');
+# @todo: make symlink to that folder
+symlink($assets, getenv('ZS_APPLICATION_BASE_DIR').'/public/assets/');
+*/
